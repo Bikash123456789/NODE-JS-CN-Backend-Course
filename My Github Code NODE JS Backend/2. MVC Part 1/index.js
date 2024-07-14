6e0061f9ace04a5711d6d687625042c2c6b29639
Bikash <medhi01011997@gmail.com>
2024-07-14 10:58:39 +0530

    CODE 2d - Setting Up and using Template Engines in Inventory Project

--- My Github Code NODE JS Backend/2. MVC Part 1/index.js
+++ My Github Code NODE JS Backend/2. MVC Part 1/index.js
@@ -1,8 +1,15 @@
 import express from 'express';
 import path from 'path';
+import expressLayouts from "express-ejs-layouts"
 import ProductsController from './src/controllers/products.controller.js';
 
 const server = express()
+
+server.set('view engine','ejs')
+server.set('views',path.join(path.resolve(),'src','views'))
+
+
+server.use(expressLayouts);
 
 //instance of ProductsController
 const productController = new ProductsController()
--- My Github Code NODE JS Backend/2. MVC Part 1/src/controllers/products.controller.js
+++ My Github Code NODE JS Backend/2. MVC Part 1/src/controllers/products.controller.js
@@ -5,6 +5,7 @@ export default class ProductsController {
     getProducts(req,res){
         let products = ProductModel.get()
         console.log(products)
-        res.sendFile(path.join(path.resolve(),"src",'views','products.html'))
+        // res.sendFile(path.join(path.resolve(),"src",'views','products.html'))
+        res.render('products',{products:products})
     }
 }
