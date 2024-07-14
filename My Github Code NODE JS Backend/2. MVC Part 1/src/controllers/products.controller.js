23d6c70577132b011b46ccdff2cbcaff3838c2e3
Bikash <medhi01011997@gmail.com>
2024-07-14 10:44:56 +0530

    CODE 2c - Creating Models in Inventory Project

--- My Github Code NODE JS Backend/2. MVC Part 1/src/controllers/products.controller.js
+++ My Github Code NODE JS Backend/2. MVC Part 1/src/controllers/products.controller.js
@@ -1,7 +1,10 @@
-import path from "path"
+import path from "path";
+import ProductModel from "../models/product.model.js";
 
 export default class ProductsController {
     getProducts(req,res){
+        let products = ProductModel.get()
+        console.log(products)
         res.sendFile(path.join(path.resolve(),"src",'views','products.html'))
     }
 }
