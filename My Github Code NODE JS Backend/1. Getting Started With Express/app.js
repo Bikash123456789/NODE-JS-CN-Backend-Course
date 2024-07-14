d7521286bec7e91d87538c07e0083173948c0729
Bikash <medhi01011997@gmail.com>
2024-07-14 06:06:08 +0530

    CODE 1c - HTTP Headers and Status Codes

--- My Github Code NODE JS Backend/1. Getting Started With Express/app.js
+++ My Github Code NODE JS Backend/1. Getting Started With Express/app.js
@@ -2,21 +2,10 @@ const express = require("express");
 
 const app = express()
 
-//Middleware1
-app.get("/",(req,res,next)=>{
-    console.log("Hello from middleware1")
-    next()
-})
 
-//Middleware2
-app.get("/",(req,res,next)=>{
-    console.log("Hello from middleware2")
-    next()
-})
-
-//Middleware3
-app.get("/",(req,res,next)=>{
-    res.send("Ending response from middleware3")
+app.get("/",(req,res)=>{
+    res.set('Content-Type','text/plain')
+    res.status(201).send("Ending response from middleware3")
 })
 
 app.listen(5000,()=>{
