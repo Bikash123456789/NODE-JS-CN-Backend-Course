d5ca9aefe14b575587fa875b6720f67e4908e0f6
Bikash <medhi01011997@gmail.com>
2024-07-14 10:37:20 +0530

    CODE 2b - Creating Controllers in Inventory Project

--- My Github Code NODE JS Backend/2. MVC Part 1/index.js
+++ My Github Code NODE JS Backend/2. MVC Part 1/index.js
@@ -1,13 +1,15 @@
 import express from 'express';
-import path from 'path'
+import path from 'path';
+import ProductsController from './src/controllers/products.controller.js';
 
 const server = express()
 
+//instance of ProductsController
+const productController = new ProductsController()
+
 server.use(express.static('src/views')) // Check at http://localhost:3400/products.html
 
-server.get("/",(req,res)=>{
-    res.sendFile(path.join(path.resolve(),'src','views','products.html'))
-})
+server.get("/",productController.getProducts)
 
 server.listen(3400,()=>{
     console.log("Server is listening at port 3400")
