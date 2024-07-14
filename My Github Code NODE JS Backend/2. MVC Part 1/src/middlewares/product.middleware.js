9ae43d9cdaa4a56a34c8746dd835c3f1d909b51d
Bikash <medhi01011997@gmail.com>
2024-07-14 20:43:03 +0530

    CODE 3d : Express Validator third party library

--- My Github Code NODE JS Backend/2. MVC Part 1/src/middlewares/product.middleware.js
+++ My Github Code NODE JS Backend/2. MVC Part 1/src/middlewares/product.middleware.js
@@ -1,25 +1,26 @@
-const validateRequest = (req, res, next) => {
-    let {name, price, imageUrl} = req.body
-    let errors = []
-    
-    
-    if(!name || name.trim() == '')
-    errors.push('Name is invalid')
-    
-    
-    if(price < 0)
-    errors.push('Price is invalid')
-    
-    
-    try{
-    const validURL = new URL(imageUrl)
-    }catch(e){
-    errors.push(e)
-    }
-    if(errors.length != 0)
-    res.render('new-product', { errorMsg: errors[0] })
-    else
-    next()
-    }
-    
-export default validateRequest    
+import {body, validationResult} from 'express-validator'
+
+
+const validateRequest = async (req, res, next) => {
+//Step 1: Setup rules for validation
+const rules = [
+body('name').isLength({min : 1}).withMessage('Name is required'),
+body('imageUrl').isURL().withMessage('Image URL is missing'),
+body('price').isInt({min : 1}).withMessage('Price should be greater than 1')
+]
+//Step 2: Run the rules
+await Promise.all(rules.map((rule) => rule.run(req)))
+let validationErrors = validationResult(req)
+
+
+console.log(validationErrors)
+//Check if there was any validation error
+if(!validationErrors.isEmpty()){
+res.render('new-product', { errorMsg: validationErrors.array()[0].msg
+})
+}else{
+next()
+}
+}
+export default validateRequest
+
