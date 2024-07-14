a184f6f29a6be183f0516a1277ed899132f07372
Bikash <medhi01011997@gmail.com>
2024-07-14 10:06:25 +0530

    CODE 2a2 - Creating Views in Inventory Project

--- My Github Code NODE JS Backend/2. MVC Part 1/index.js
+++ My Github Code NODE JS Backend/2. MVC Part 1/index.js
@@ -1,8 +1,13 @@
-import express from 'express'
+import express from 'express';
+import path from 'path'
 
 const server = express()
 
 server.use(express.static('src/views')) // Check at http://localhost:3400/products.html
+
+server.get("/",(req,res)=>{
+    res.sendFile(path.join(path.resolve(),'src','views','products.html'))
+})
 
 server.listen(3400,()=>{
     console.log("Server is listening at port 3400")
